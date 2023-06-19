ae458b7c8f52f3b8a8472b4e828d46ae4713fcaf
mstrmindz <47842133+mstrmindz@users.noreply.github.com>
2023-06-19 20:07:27 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-            headers = {"User-Agent": "My Reddit Scraper"}  # Set a custom User-Agent header
-
-                response = requests.get(url, headers=headers, allow_redirects=False)
-                    if response.status_code == 200:
-                                data = response.json()
-                                        posts = data["data"]["children"]
-                                                for post in posts:
-                                                                title = post["data"]["title"]
-                                                                            print(title)
-                                                                                else:
-                                                                                            print("None")
-
-                                                                                            # Example usage
-                                                                                            subreddit = "python"
-                                                                                            top_ten(subreddit)
-
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
