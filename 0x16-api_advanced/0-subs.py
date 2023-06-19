83d579ee088810582c58c4e83eead3a2eabf4a41
mstrmindz <47842133+mstrmindz@users.noreply.github.com>
2023-06-19 19:47:01 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-
+"""
+queries the Reddit API and returns the number of subscribers(not active
+users, total subscribers) for a given subreddit. if an invalid subreddit
+is given, the function should return 0.
+"""
 import requests
 
-def number_of_subscribers(subreddit):
-        url = f"https://www.reddit.com/r/{subreddit}/about.json"
-            headers = {"User-Agent": "My Reddit Scraper"}  # Set a custom User-Agent header
 
-                response = requests.get(url, headers=headers, allow_redirects=False)
-                    if response.status_code == 200:
-                                data = response.json()
-                                        subscribers = data["data"]["subscribers"]
-                                                return subscribers
-                                                else:
-                                                            return 0
+def number_of_subscribers(subreddit):
+    """queries the Reddit API"""
+    headers = {
+        "User-Agent": "0x16. API_advanced-e_kiminza"
+        }
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    sub = response.json().get("data").get("subscribers")
+    return sub
