b069ca9e29c57ec88cd54282f8c20d6076e3cd21
mstrmindz <47842133+mstrmindz@users.noreply.github.com>
2023-06-23 13:49:55 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fix 500 error when a GET HTTP method is requested to Apache web server
+# Puppet manifest to fix a bug in wp-setings.php
 
-exec {'replace':
-  provider => shell,
-    command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-    }
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
