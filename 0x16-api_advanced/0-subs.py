e3510e3d49d9095386533114f05fe53baff6769f
mstrmindz <47842133+mstrmindz@users.noreply.github.com>
2023-08-08 19:26:17 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python3
-
 import requests
-import sys
 
 def number_of_subscribers(subreddit):
     # Set a custom User-Agent to avoid Too Many Requests errors
@@ -11,22 +8,13 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
-        elif response.status_code == 404:
-            # Subreddit not found
-            return 0
         else:
-            # Other error occurred
-            print(f"Error: {response.status_code} - {response.text}")
             return 0
     except requests.exceptions.RequestException as e:
         print(f"Request Exception: {e}")
         return 0
-
-# Test the function
-subreddit_name = 'python'
-subscribers_count = number_of_subscribers(subreddit_name)
-print(f"The number of subscribers in r/{subreddit_name}: {subscribers_count}")
